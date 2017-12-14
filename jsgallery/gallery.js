70135fb3ca2fa613a22754a331e1b7cdc9b9f529
“Yeiosn <jeison.zk6@hotmail.com>
2017-12-14 16:04:34 -0500

    adding image overlapping

--- jsgallery/gallery.js
+++ jsgallery/gallery.js
@@ -5,7 +5,7 @@ export class Gallery {
         this.elements = {}
         
         this.setframework()
-        this.setDots_dynamically(data)
+        this.setlist_dot(data)
         this.setlist_image(data) 
         this.setEvents()
     }
@@ -37,9 +37,16 @@ export class Gallery {
       return {
         list_image:(
           `<li class=gallery__style-dots-images>
-            <img class=gallery__images src="https://my.ifdesign.de/upload/award_img_309/oex_large/176468_01_01_Ferrari_FXX_K.jpg"/> 
+            <img class=gallery__images src=""/> 
           </li>`
         )
+      }
+    }
+
+    static get states_elements () {
+      return {
+        dotSelected: 'gallery__dots--selected',
+        imageSelected: 'gallery___style-dots-images--selected'
       }
     }
 
@@ -52,37 +59,41 @@ export class Gallery {
         //this.elements.rightBtn = this.node.querySelector('.gallery__arrow--right') 
     }
 
-    setDots_dynamically (data) {
-         var dot_html=Gallery.templates.list_dot
-         var array_dots_html= new Array(data.length);
+    setlist_dot (data) {
+         const dot_html=Gallery.templates.list_dot
+         const array_dots_html= new Array(data.length);
          const dots_html=array_dots_html.fill(dot_html).join('')
          this.elements.dotsContainer.innerHTML = dots_html
-         this.elements.dots = this.elements.dotsContainer.querySelectorAll('button.gallery__dots')
+         this.elements.dots = this.elements.dotsContainer.querySelectorAll('.gallery__dots')
+         this.elements.dots[this.index].classList.add(Gallery.states_elements.dotSelected)
     }
 
     setlist_image (data) {
-        var image_html=Gallery.list_image().list_image
-        var array_images_html=new Array(data.length)
+        const image_html=Gallery.list_image().list_image
+        const array_images_html=new Array(data.length)
         const images_html=array_images_html.fill(image_html).join('')
         this.elements.imagesContainer.innerHTML = images_html
+        this.elements.image=this.node.querySelectorAll('.gallery__images')
+        this.elements.image.forEach( (element,index)=> {
+          this.elements.image[index].src=data[index].url
+        })
+        this.elements.image[this.index].classList.add(Gallery.states_elements.imageSelected)
   }
 
-
   setEvents () {
-    this.elements.dotsContainer.addEventListener('click', this.dotHandler.bind(this))
+    this.elements.dotsContainer.addEventListener('click', this.dotDriver.bind(this))
     // this.elements.rightBtn.addEventListener('click', this.goNext.bind(this))
     // this.elements.leftBtn.addEventListener('click', this.goPrevious.bind(this))
     // this.node.addEventListener('keydown', this.keydownHandler.bind(this))
   }
 
-  dotHandler (event) {
+  dotDriver (event) {
     const clickedElement = event.target
     const tag_clicked = clickedElement.nodeName
-    const nodelistToArray= Array.apply(null, this.elements.dots);
-
+    const nodelistToArray= Array.apply(null, this.elements.dots)
     if(tag_clicked=='BUTTON'){
       const index = nodelistToArray.indexOf(clickedElement)
-      console.log(index)
+      this.changeImage_gallery(index)
      }
   }
 
